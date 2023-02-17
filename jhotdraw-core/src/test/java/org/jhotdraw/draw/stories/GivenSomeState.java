f7c1dd1b7efd2a000af7cc0b7a2c52fc177e23ff
MizzleTheHorse <emil.madsen@live.dk>
2023-02-17 11:48:21 +0100

    Added Jgiven user stories. Added workflow test file.

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/stories/GivenSomeState.java
@@ -0,0 +1,17 @@
+package org.jhotdraw.draw.stories;
+
+import com.tngtech.jgiven.Stage;
+
+public class GivenSomeState extends Stage<GivenSomeState> {
+
+
+    public GivenSomeState old_text_need_change() {
+        return self();
+    }
+
+    public GivenSomeState text_to_write_to_canvas() {
+        return self();
+    }
+}
+
+
