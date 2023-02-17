cd2a9a972e6f8e86d14d84fa8721cab6704634ca
OskarT <ostho20@student.sdu.dk>
2023-02-17 01:24:04 +0100

    Refactoring

--- jhotdraw-actions/src/main/java/org/jhotdraw/action/edit/ActionHelper.java
+++ jhotdraw-actions/src/main/java/org/jhotdraw/action/edit/ActionHelper.java
@@ -1,2 +1,21 @@
-package org.jhotdraw.action.edit;public class ActionHelper {
+package org.jhotdraw.action.edit;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ActionHelper {
+    public ActionHelper(){}
+
+    public JComponent TargetCheck(JComponent target) {
+        JComponent c = target;
+
+        KeyboardFocusManager keyboardFocusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        if (c == null && (KeyboardFocusManager.getCurrentKeyboardFocusManager().
+                getPermanentFocusOwner() instanceof JComponent)) {
+            c = (JComponent) KeyboardFocusManager.getCurrentKeyboardFocusManager().
+                    getPermanentFocusOwner();
+        }
+
+        return c;
+    }
 }
